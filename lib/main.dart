3d398a463a2ea9bcab16cea56467cdea0f05fb09
crowjambo <evaldascrow@gmail.com>
2020-10-31 22:28:05 +0200

    reset

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: TestScreen(),
+      home: HomeScreen(),
     );
   }
 }
