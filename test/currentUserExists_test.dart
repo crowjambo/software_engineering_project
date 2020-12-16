e4779079e18520516e88e3aa4c80e6083d3a2b41
Saulė Stankevičiūtė <vogellietuva@gmail.com>
2020-12-16 20:54:53 +0200

    currentUserExistst tests written

---
+++ test/currentUserExists_test.dart
@@ -0,0 +1,39 @@
+import 'package:test/test.dart';
+import 'package:software_engineering_project/models/user_model.dart';
+import 'package:software_engineering_project/main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:software_engineering_project/utility/globals.dart' as globals;
+
+var mockUser = User("testUsername", "123", "00:00");
+
+void main(){
+  group('LocalStorage User Registration', () {
+
+    test('When user exists Then currentUserExists return true', () async {
+      SharedPreferences.setMockInitialValues(<String, dynamic>{"userRegistered": true});
+
+      var result = await currentUserExists();
+      expect(result, true);
+    });
+
+    test('When user does not exist Then currentUserExists return false', () async {
+      SharedPreferences.setMockInitialValues(<String, dynamic>{"userRegistered": false});
+
+      var result = await currentUserExists();
+      expect(result, false);
+    });
+
+    test('When user is registered Then userName is stored globally', () async {
+      SharedPreferences.setMockInitialValues(<String, dynamic>{
+        "currentUserName": mockUser.userName,
+        "currentUUID": mockUser.uuID,
+      });
+
+      await currentUserExists();
+
+      var result = globals.currentUser.userName;
+      expect(result, mockUser.userName);
+    });
+
+  });
+}
