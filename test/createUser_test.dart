860f8fc6fef43ccc11f6756783772b279aaa3e5c
Saulė Stankevičiūtė <vogellietuva@gmail.com>
2020-12-17 20:26:22 +0200

    added createUser test

--- test/createUser_test.dart
+++ test/createUser_test.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:test/test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -10,16 +8,24 @@ import 'package:software_engineering_project/controllers/user_controller.dart';
 var mockUser = User("testUsername", "123", "00:00", "privateKey");
 
 void main() {
-  group('LocalStorage User Removal', () {
+  group('LocalStorage User Creation', () {
 
-    test('When user is deleted Then it is removed from local storage', () async {
-      SharedPreferences.setMockInitialValues(<String, dynamic>{
-        "currentUserName": mockUser.userName,
-        "currentUUID": mockUser.uuID,
-        "RSA_private_key": "privateKey123",
-        "RSA_public_key": "publicKey123",
-        "userRegistered": true
-      });
+    test('When user is created Then it is added to local storage', () async {
+      // SharedPreferences.setMockInitialValues(<String, dynamic>{
+      //   "currentUserName": mockUser.userName,
+      //   "currentUUID": mockUser.uuID,
+      //   "RSA_private_key": "privateKey123",
+      //   "RSA_public_key": "publicKey123",
+      //   "userRegistered": true
+      // });
+
+      // Create user
+      createUserInLocalStorage(mockUser.userName);
+
+      // Check if user is created
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      expect(prefs.getString("currentUserName"), mockUser.userName);
+      expect(prefs.getBool("userRegistered"), true);
 
       // Check for the user to be in the local storage
       var userCreated = await currentUserExists();
@@ -29,7 +35,6 @@ void main() {
       await deleteUserInLocalStorage();
 
       // Checks if user was deleted
-      SharedPreferences prefs = await SharedPreferences.getInstance();
       expect(prefs.getString("currentUUID"), null);
       expect(prefs.getString("currentUserName"), null);
       expect(prefs.getString("RSA_private_key"), null);
