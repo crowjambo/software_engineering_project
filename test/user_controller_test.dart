d712ebb7e7f342f67a744f0f8bc4e8c82e96b3d9
crowjambo <evaldascrow@gmail.com>
2021-01-09 16:15:09 +0200

    files

---
+++ test/user_controller_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:software_engineering_project/controllers/user_controller.dart';
+import 'package:software_engineering_project/utility/globals.dart' as globals;
+
+void main() {
+  test('', () async {});
+}
